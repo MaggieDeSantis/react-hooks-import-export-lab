1cac72b777ad563ee6eb0a1bcdc1fe033cb9d08d
MaggieDeSantis <sheisntreal@gmail.com>
2023-05-21 16:20:55 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { image1 } from '../data/user';
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={image1} alt="I made this" />
     </div>
   );
 }
+export default About;
